89e3bfe9691e8c92e529f288bce8f9c1a5a580a4
villimin <54509650+villimin@users.noreply.github.com>
2021-11-02 10:54:57 -0700

    Update ForecastCalculator.cs

--- ICE_Villy_Test/ForecastCalculator.cs
+++ ICE_Villy_Test/ForecastCalculator.cs
@@ -14,7 +14,7 @@ namespace ICE_Villy_Test
 {
     public static class ForecastCalculator
     {
-        public const float KELVIN_TO_CELSIUS = -273.15f;
+        public const float KELVIN_TO_CELSIUS = 273.15f;
         public static readonly string GRIB_CLI_EXE_PATH = $"{Config.ROOT_FOLDER_PATH}wgrib2\\wgrib2.exe";
 
         public static float GetCelcius(string response)
