df62432ee951d2a47a4069388a2047ac5c29d4d6
villimin <54509650+villimin@users.noreply.github.com>
2021-11-04 04:14:59 -0700

    Update FileManager.cs

--- ICE_Villy_Test/FileManager.cs
+++ ICE_Villy_Test/FileManager.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -22,18 +22,31 @@ namespace ICE_Villy_Test
             client = new WebClient();
         }
 
-        public static bool DownloadFileIfNotExists(DateTime forecastDate, DateTime currentDate, out string errorMessage, ref string filePath)
+        public static bool DownloadFileIfNotExists(DateTime forecastDate, out string errorMessage, ref string filePath)
         {
             errorMessage = string.Empty;
 
             try
             {
-                int offsetDays = (int)(forecastDate - currentDate).TotalHours;
-                string time = currentDate.ToString("yyyymmdd");
+                int offsetDays = 0;
+                DateTime currentTime = DateTime.Now;
+                string timeStr = string.Empty;
+
+                if(forecastDate > currentTime)
+                {
+                    offsetDays = (int)(forecastDate - DateTime.Now).TotalHours;
+                    timeStr = currentTime.ToString("yyyyMMdd");
+                }
+                else
+                {
+                    offsetDays = (int)(DateTime.Now - forecastDate).TotalHours;
+                    timeStr = forecastDate.ToString("yyyyMMdd");
+                }
+
                 string offset = offsetDays.ToString("D3");
 
                 string url = URL;
-                url = url.Replace("{time}", time);
+                url = url.Replace("{time}", timeStr);
                 url = url.Replace("{offset}", offset);
 
                 string fileName = GetFileNameFromUrl(url);
